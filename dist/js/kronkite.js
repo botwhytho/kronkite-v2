961d14413fd5a39e94ddbed2d6bbdf29944eab66
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2017-03-19 21:09:38 -0400

    Utility commit.

--- dist/js/kronkite.js
+++ dist/js/kronkite.js
@@ -876,12 +876,12 @@ Container.modules["videos-feed"] = function({require, set}) {
 
 new Container(["*"], function start(APP) {
 	APP.start(["config", "router", "model", "core-events"])({
-		environment: "debug",
+		environment: "production",
 		remoteDebug: false,
 		routeMap: {
 			debug: "http://localhost:8080/",
 			remoteDebug: "http://192.168.254.4:8080/",
-			production: "http://kronkite-server.herokuapp.com"
+			production: "https://kronkite-server.herokuapp.com/"
 		}	
 	});
 });
